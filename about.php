ebec47c19892d176d22d232a9d677f3c7322780a
Vasil Salkou <82231299+SaivanKarnet@users.noreply.github.com>
2021-04-11 16:04:47 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>О Салькова Василия</title>
+<meta charset="utf-8">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>О Васе</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Как же он стал программистом?
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>О Васе</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 Вася родился 04 октября 2010 года. Он с 8-ми лет очень увлекался компьютерами и не случайно он в 10 лет создал свой YouTube-канал и instagram saivan_karnet. Первым его языком стал Python. Сейчас он учит языки: Python, PHP (На которым создан этот сайт), HTML и CSS. А ещё он очень хорошо заберается в компьютерах ОС как: Windows и Mac.
+						
